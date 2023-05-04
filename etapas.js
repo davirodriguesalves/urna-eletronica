6afea06d53882be692c1ebf3eb4e20313d34b992
DaviRodriguesAlves <davirodriguesalves01@gmail.com>
2023-05-03 22:06:14 -0300

    Projeto Urna Eletrônica

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '01234',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'candidato-03.png', legenda: 'Vereador', samll: false}
+                ]
+            },
+            {
+                numero: '56789',
+                nome: 'Beltrano da Silva',
+                partido: 'DEF',
+                fotos:[
+                    {url:'candidato-02.png', legenda: 'Vereador'}
+                ]
+            },
+        
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome:'Ciclano',
+                partido: 'ABC',
+                vice: 'Diclano',
+                fotos: [
+                    {url: 'candidato-01.png', legenda: 'Prefeito'},
+                    {url: 'candidato-04.png', legenda: 'Vice-Prefeito', samll: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome:'Zulano',
+                partido: 'UAI',
+                vice: 'Ficlano',
+                fotos: [
+                    {url: 'candidato-06.png', legenda: 'Prefeito'},
+                    {url: 'candidato-05.png', legenda: 'Vice-Prefeito', samll: true}
+                ]
+            },
+        ]
+    }
+]
